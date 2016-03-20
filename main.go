a396a08f8b781c69036fc424bab42deb4b5ee819
sysradium <sysradium@users.noreply.github.com>
2016-03-21 01:22:30 +0300

    Add startup flags

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/syradium/libvirt_rest_go/api"
 	"github.com/syradium/libvirt_rest_go/manager"
 )
 
+var (
+	pool     = flag.String("pool", "default", "Pool name to use for VPS storage")
+	poolPath = flag.String("pool-path", "/root/images", "Path to VPS images")
+)
+
 func main() {
-	conn, err := manager.NewLibvirtFacade("qemu:///system", "mypool", "/root/images")
+	flag.Parse()
+	conn, err := manager.NewLibvirtFacade("qemu:///system", *pool, *poolPath)
 	if err != nil {
 		log.Fatal(err)
 		return
